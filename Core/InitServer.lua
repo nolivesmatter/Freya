5d8b5ffc21138380a024691beec85a7136b388ba
Skye <exp.lunate@gmail.com>
2016-09-19 19:03:14 +0100

    Create InitServer.lua

---
+++ Core/InitServer.lua
@@ -0,0 +1,37 @@
+--//
+--// * Init script for Freya Server
+--// | Make sure everything actually loads
+--//
+
+-- Get our Freya Main
+local Freya = require(game.ServerStorage.Freya.Main);
+
+-- Put it in _G
+_G.Freya = Freya
+_G.FreyaServer = Freya
+
+-- Load everything in ServerScriptService
+-- Respect load orders
+local loadtable = {};
+for k,v in next, script.Parent:GetChildren() do
+  if v ~= script then
+    if v.Enabled.Value then
+      loadtable[#loadtable+1] = v;
+    end
+  end
+end
+table.sort(loadtable, function(a,b)
+  local loada = a:FindFirstChild("LoadOrder")
+  loada = loada and loada.Value or 1
+  local loadb = b:FindFirstChild("LoadOrder")
+  loadb = loadb and loadb.Value or 1
+  return loada < loadb
+end);
+for i=1, #loadtable do
+  local v = loadtable[i];
+  local ready = v:FindFirstChild("Ready");
+  v.Disabled = false;
+  if ready then
+    while not ready.Value do ready.Changed:wait() end
+  end
+end
