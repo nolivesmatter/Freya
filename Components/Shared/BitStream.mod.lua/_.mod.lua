803224896e49b2e060be6bc00c9590b3e9b6a73c
Skye <exp.lunate@gmail.com>
2016-09-27 22:30:51 +0100

    Update and rename Components/Shared/Encode.mod.lua to Components/Shared/BitStream.mod.lua/_.mod.lua

--- Components/Shared/BitStream.mod.lua/_.mod.lua
+++ Components/Shared/BitStream.mod.lua/_.mod.lua
@@ -101,7 +101,8 @@ end
 
 local base64bytes = {['A']=0,['B']=1,['C']=2,['D']=3,['E']=4,['F']=5,['G']=6,['H']=7,['I']=8,['J']=9,['K']=10,['L']=11,['M']=12,['N']=13,['O']=14,['P']=15,['Q']=16,['R']=17,['S']=18,['T']=19,['U']=20,['V']=21,['W']=22,['X']=23,['Y']=24,['Z']=25,['a']=26,['b']=27,['c']=28,['d']=29,['e']=30,['f']=31,['g']=32,['h']=33,['i']=34,['j']=35,['k']=36,['l']=37,['m']=38,['n']=39,['o']=40,['p']=41,['q']=42,['r']=43,['s']=44,['t']=45,['u']=46,['v']=47,['w']=48,['x']=49,['y']=50,['z']=51,['0']=52,['1']=53,['2']=54,['3']=55,['4']=56,['5']=57,['6']=58,['7']=59,['8']=60,['9']=61,['-']=62,['_']=63,['=']=nil}
 
-function dec64(data)
+function dec64(...)
+	data = Hybrid(...)
 	local chars = {}
 	local result=""
 	for dpos=0,string.len(data)-1,4 do
@@ -110,6 +111,20 @@ function dec64(data)
 	end
 	return result
 end
+
+local Controller = {
+  fromB64 = dec64; -- Wrap as a BitStream later
+  new = function(stub) end; -- Create a BitStream object later.
+  -- Should allow for optimisation of bitwise operations.
+  -- Note, use ee5 b64 and port for pure Lua 5.1
+};
+local ni = newproxy(true);
+local mt = getmetatable(ni);
+mt.__index = Controller;
+mt.__tostring = function() return "Freya BitStream Controller" end
+mt.__metatable = "Locked metatable: Freya";
+
+Hybrid = function(...) if ... == ni then return select(2, ...) else return ... end end
 
 return {
   b64 = enc64;
